0c0fdeb62c3aca38d844dcae7804fa6dab76b4dc
Ameed-Saleh <ameed.saleh@outlook.com>
2026-03-13 15:22:27 +0200

    Homework - Completed!

--- 11-03-26_HW/Q2.py
+++ 11-03-26_HW/Q2.py
@@ -56,6 +56,7 @@ def get_statistics(numbers)->dict:
         'avg': statistics.mean(numbers),
         'min': min(numbers),
         'max': max(numbers),
+        'length': len(numbers)
     }
     return _result
 
